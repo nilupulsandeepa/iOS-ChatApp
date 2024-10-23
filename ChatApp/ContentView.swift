df5ea6adfdedba5f98be2086d17e5493a214128f
Nilupul Sandeepa <nilupulsandeepa@gmail.com>
2024-10-23 17:59:49 +0530

    initial commit

---
+++ ChatApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ChatApp
+//
+//  Created by Nilupul Sandeepa on 2024-10-23.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
